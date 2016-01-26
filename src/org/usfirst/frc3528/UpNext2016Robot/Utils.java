302187331dc3867b40f0ba4c6e71dd015597399c
Brainiac98 <njlh98@icloud.com>
2016-01-25 21:07:51 -0600

    Added Utils

---
+++ src/org/usfirst/frc3528/UpNext2016Robot/Utils.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc3528.UpNext2016Robot;
+
+public class Utils {
+
+	//A method used for smooth driving
+	public static double rampSpeed(double input) {
+    	//auto set sensitivity to .5
+    	return rampSpeed(input, .5);
+	}	
+
+
+	public static double rampSpeed(double input, double sensitivity) {
+
+    	if (IsInDeadband(input)) {
+    		return 0;
+    }
+    	if (IsAtTop(input)) {
+    		return .75;
+    	}
+    	
+    	if(IsAtBottom(input)) {
+    		return -.75;
+    	}
+
+    	//formula for ramping: f(x) = ax^3 + (1-a)x where a is the sensitivity and x is the input
+    	return (sensitivity * input * input * input + (1 - sensitivity) * input);
+	}
+
+
+	private static boolean IsInDeadband(double input) {
+		return input > -.1 && input < .1;
+	}
+
+	
+	private static boolean IsAtTop(double input) {
+		return input >= .75;
+	}
+	
+	
+	private static boolean IsAtBottom(double input) {
+		return input <= -.75; 
+	}
+}
