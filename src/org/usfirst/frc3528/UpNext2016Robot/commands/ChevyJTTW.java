738c2b61bef861fd85f45dcec3399345152ba03b
Brainiac98 <njlh98@icloud.com>
2016-02-29 17:55:46 -0600

    Tweaked auto code for Cheval and Portcullis.

--- src/org/usfirst/frc3528/UpNext2016Robot/commands/ChevyJTTW.java
+++ src/org/usfirst/frc3528/UpNext2016Robot/commands/ChevyJTTW.java
@@ -9,7 +9,7 @@ public class ChevyJTTW extends CommandGroup {
     
     public  ChevyJTTW() {
     	
-    	addSequential ( new MoveArmToPosition(1, 5, 1630) );
+    	addSequential ( new MoveArmToPosition(1, 5, 1780) );
     	addSequential ( new DriveByEncoderCounts(1400, 5, -0.6) );
     	addParallel ( new DriveByEncoderCounts(1200, 7, -0.6) );
     	addSequential ( new MoveArmToPosition(1, 5, 400) );
